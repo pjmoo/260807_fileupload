1a62d5fc13524e20e26132eb6acb1fa4355e6213
pjmoo <00pjmoooo@gmail.com>
2026-08-07 10:13:52 +0900

    feat: 파일 업로드 폼 및 POST 요청 처리 핸들러 구현

--- src/main/java/org/example/fileupload/controller/FileController.java
+++ src/main/java/org/example/fileupload/controller/FileController.java
@@ -2,7 +2,11 @@ package org.example.fileupload.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/files")
@@ -10,5 +14,13 @@ public class FileController {
     @GetMapping("/new")
     public String filesForm() {
         return "files/form";
+    }
+
+    @PostMapping("/new")
+    public String uploadFile(
+            @RequestParam MultipartFile image,
+            RedirectAttributes ra) {
+        System.out.println("image = " + image);
+        return "redirect:/files/new"; // PRG
     }
 }
