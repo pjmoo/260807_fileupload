004d7d3f924af9c5258d89253ca9549b511acff6
pjmoo <00pjmoooo@gmail.com>
2026-08-07 10:51:05 +0900

    refactor: FileStore 유틸리티로 파일 저장 로직 분리 및 FileController 리팩토링

--- src/main/java/org/example/fileupload/controller/FileController.java
+++ src/main/java/org/example/fileupload/controller/FileController.java
@@ -1,5 +1,8 @@
 package org.example.fileupload.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.fileupload.dto.UploadFile;
+import org.example.fileupload.util.FileStore;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,12 +11,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 @Controller
 @RequestMapping("/files")
+@RequiredArgsConstructor
 public class FileController {
+    // import org.example.fileupload.util.FileStore;
+    private final FileStore fileStore;
+
     @GetMapping("/new")
     public String filesForm() {
         return "files/form";
@@ -23,23 +27,25 @@ public class FileController {
     public String uploadFile(
             @RequestParam MultipartFile image,
             RedirectAttributes ra) {
-        System.out.println("image = " + image);
-        if (image.isEmpty()) {
-            ra.addFlashAttribute("msg", "파일을 넣어주세요.");
-            return "redirect:/files/new";
-        }
+//        System.out.println("image = " + image);
+//        if (image.isEmpty()) {
+//            ra.addFlashAttribute("msg", "파일을 넣어주세요.");
+//            return "redirect:/files/new";
+//        }
         // 파일 업로드
-        Path uploadPath = Path.of("src/main/resources/static/upload"); // 연습용
+//        Path uploadPath = Path.of("src/main/resources/static/upload"); // 연습용
         // 경로가 없으면 에러가 터짐
-        try {
-            Files.createDirectories(uploadPath); // 없으면 만들어줌
-            // 파일 저장용 경로가 필요
-            Path filePath = uploadPath.resolve(image.getOriginalFilename());
-            image.transferTo(filePath);
-        } catch (Exception e) {
-            throw new RuntimeException(e); // 접근권한 혹은 파일 시스템 오류
-        }
-        ra.addFlashAttribute("msg", "업로드 성공 (%s)".formatted(image.getOriginalFilename()));
-        return "redirect:/files/new"; // PRG
+//        try {
+//            Files.createDirectories(uploadPath); // 없으면 만들어줌
+//            // 파일 저장용 경로가 필요
+//            Path filePath = uploadPath.resolve(image.getOriginalFilename());
+//            image.transferTo(filePath);
+//        } catch (Exception e) {
+//            throw new RuntimeException(e); // 접근권한 혹은 파일 시스템 오류
+//        }
+//        ra.addFlashAttribute("msg", "업로드 성공 (%s)".formatted(image.getOriginalFilename()));
+        UploadFile uploadFile = fileStore.storeFile(image);
+        ra.addFlashAttribute("result", uploadFile);
+        return "redirect:/files/new";
     }
 }
--- src/main/java/org/example/fileupload/util/FileStore.java
+++ src/main/java/org/example/fileupload/util/FileStore.java
@@ -1,7 +1,36 @@
 package org.example.fileupload.util;
 
+import org.example.fileupload.dto.UploadFile;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
 
 @Component
 public class FileStore {
+    private final Path uploadPath = Path.of("src/main/resources/static/upload");
+
+    public UploadFile storeFile(MultipartFile image) {
+        // 1. 비어 있는지 검증
+        if (image == null || image.isEmpty()) {
+            throw new IllegalArgumentException("파일이 비어 있습니다.");
+        }
+        String originalName = Objects.requireNonNull(image.getOriginalFilename());
+        // 1.1. 안전한 파일명 처리
+        String safeName = StringUtils.cleanPath(originalName);
+        // 1.2. 확장자 (TO DO)
+
+        // 2. 디렉터리 준비
+        try {
+            Files.createDirectories(uploadPath);
+            Path filePath = uploadPath.resolve(safeName).normalize(); // 추가적인 파일명, 경로 방어
+            image.transferTo(filePath);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return new UploadFile(originalName, safeName);
+    }
 }
